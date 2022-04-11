20f051ede2d41a0ad63f6b4bc95240c5f1e80272
Sadid369 <sadid.jones@gmail.com>
2022-04-11 21:17:50 +0600

    again

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
-    public static void main (String[] args){
-        System.out.println("Hello World");
+
+    public static void main(String[] args){
+      System.out.println("hello Wrold");
+      int myFirstNumber = 5;
+      System.out.println(myFirstNumber);
     }
 }
